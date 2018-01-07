f9d0b06dc3ee0646f451be531c8bd9f97d695833
ksb <rnjstjdqhd3939@gmail.com>
2018-01-07 20:14:45 +0900

    test

--- mutil-task-service/src/main/java/com/example/amqp/Application.java
+++ mutil-task-service/src/main/java/com/example/amqp/Application.java
@@ -1,7 +1,11 @@
 package com.example.amqp;
 
+import com.example.amqp.entity.TaskTest;
+import com.example.amqp.repository.TaskTestRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +13,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner demo(TaskTestRepository taskTestRepository) {
+//		return args -> {
+//			TaskTest taskTest = new TaskTest();
+//			taskTest.setTest("taskTest setTest");
+//			taskTest.setTask("taskTest setTask");
+//			taskTest.setName("taskTest setName");
+//			taskTestRepository.save(taskTest);
+//		};
+//	}
+
 }
